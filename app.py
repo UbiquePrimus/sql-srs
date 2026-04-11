95f8b5d71615a0b70c0762837da48d1ee99c23e8
Francois <francois.dalayer@inrae.fr>
2026-04-11 22:42:28 +0200

    select first theme in queue if no theme selected in select box

--- app.py
+++ app.py
@@ -29,9 +29,18 @@ with st.sidebar:
         index=None,
         placeholder="Select your subject",
     )
-    st.write("You are studying", theme)
-
-    exercise = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'").df().sort_values("last_reviewed").reset_index()
+    if theme:
+        st.write("You are studying", theme)
+        select_exercise_query = f"SELECT * FROM memory_state WHERE theme = '{theme}'"
+    else:
+        select_exercise_query = f"SELECT * FROM memory_state"
+        st.write("No theme selected, picking first one in queue.")
+    exercise = (
+        con.execute(select_exercise_query)
+        .df()
+        .sort_values("last_reviewed")
+        .reset_index(drop=True)
+    )
     st.write(exercise)
 
 exercise_name = exercise.loc[0,"exercise_name"]
